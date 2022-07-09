9a00c564116efcd05467001d618b15bdc98731a7
Alexander von Below <alex@vonbelow.com>
2022-07-09 23:46:28 +0200

    Added a Grid Template and a simple navigation

--- CarSample/CarPlaySceneDelegate.swift
+++ CarSample/CarPlaySceneDelegate.swift
@@ -9,24 +9,46 @@ import UIKit
 // CarPlay App Lifecycle
 
 import CarPlay
+import os.log
 
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     var interfaceController: CPInterfaceController?
-
+    let logger = Logger()
+    
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
             didConnect interfaceController: CPInterfaceController) {
 
         self.interfaceController = interfaceController
-        let item = CPListItem(text: "Rubber Soul", detailText: "The Beatles")
-        let section = CPListSection(items: [item])
-        let listTemplate = CPListTemplate(title: "Albums", sections: [section])
+        
+        let gridButton = CPGridButton(titleVariants: ["Albums"],
+                                      image: UIImage(systemName: "list.triangle")!)
+        { button in
+            interfaceController.pushTemplate(self.listTemplate(),
+                                             animated: true,
+                                             completion: nil)
+
+        }
+        
+        let gridTemplate = CPGridTemplate(title: "A Grid Interface", gridButtons:  [gridButton])
+        
         // SwiftC apparently requires the explicit inclusion of the completion parameter,
         // otherwise it will throw a warning
-        interfaceController.setRootTemplate(listTemplate,
+        interfaceController.setRootTemplate(gridTemplate,
                                             animated: true,
                                             completion: nil)
     }
 
+    func listTemplate() -> CPListTemplate {
+        let item = CPListItem(text: "Rubber Soul", detailText: "The Beatles")
+        item.handler = { item, completion in
+
+            self.logger.info("Item selected")
+            completion()
+        }
+        let section = CPListSection(items: [item])
+        return CPListTemplate(title: "Albums", sections: [section])
+    }
+    
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnectInterfaceController interfaceController: CPInterfaceController) {
         self.interfaceController = nil
     }
